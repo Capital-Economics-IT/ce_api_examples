119bbe404ca31dd7c94d721ac30581255313f23c
Stuart McMath <stuart.mcmath@capitaleconomics.com>
2026-05-18 11:19:35 +0100

    Example updated

--- content_api/python/content_api_example.py
+++ content_api/python/content_api_example.py
@@ -7,15 +7,33 @@
 
 import requests
 import json
+from datetime import datetime, timedelta, timezone
+from urllib.parse import quote
 
 # Replace with your api-key
 API_KEY = '<replace with your api key>'
 
 BASE_URL = 'https://api.capitaleconomics.com/json'
 
+# Data returned is automatically filtered to items matching your subscription,
+# but can be filtered down further by specifying specific service tags.
+service_tags = [
+    'Commodities',
+    'Middle East Rapid Response',
+]
+
 params = {
     'api-key': API_KEY,
-    'page[limit]': 10,  # return the 10 most recent (default is 50, max 100)
+    # Example limit the number of items returned...
+    'page[limit]': 1,
+    # Example date filtering...
+    'filter[published_at][condition][path]': 'published_at',
+    'filter[published_at][condition][operator]': '>=',
+    'filter[published_at][condition][value]': int(datetime.fromisoformat('2026-01-01T00:00:00+00:00').timestamp()),
+    # Example filtering by specific service tags...
+    'filter[service_names][condition][path]': 'field_service.name',
+    'filter[service_names][condition][operator]': 'IN',
+    'filter[service_names][condition][value][]': service_tags
 }
 
 response = requests.get(f'{BASE_URL}/node/publication', params=params)
